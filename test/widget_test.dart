0e27119663d525f66a40c075dae2402b7c4e3cc3
Mouaad Aallam <mouaad.aallam@algolia.com>
2022-10-19 15:47:50 +0200

    rename app to main

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -8,7 +8,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
-import 'package:flutter_showcase/app.dart';
+import 'package:flutter_showcase/main.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
